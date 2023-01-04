ced18d3ad0abb2011461ae2044860cd639cdc796
eduek <eduekakpannyah@gmail.com>
2023-01-04 17:53:47 +0100

    fix: createPhoneNumber always has a return value

--- createPhoneNumber/createPhoneNumber.js
+++ createPhoneNumber/createPhoneNumber.js
@@ -1,9 +1,9 @@
 function createPhoneNumber(numbers) {
-  if (numbers && numbers.length === 10) {
-    let allNumbersEligible  = numbers.every(number => number < 10 && number >= 0);
+  if (numbers && numbers.length === 10 && Array.isArray(numbers)) {
+    let allNumbersEligible = numbers.every(number => number < 10 && number >= 0); // makes sure each digit is between 0 and 9
 
     if (allNumbersEligible) return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
   };
-};
 
-console.log(createPhoneNumber("1234567890"))
+  return null;
+};
