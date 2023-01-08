0f974e7eace06b7a05e83abe7736ce888ca24351
eduek <eduekakpannyah@gmail.com>
2023-01-08 19:30:31 +0100

    feat: day four solutions

---
+++ rgb/rgb.js
@@ -0,0 +1,37 @@
+function rgb(r, g, b) {
+  // function to convert decimal to hexadecimal
+  function toHex(number) {
+    let remainders = [];
+  
+    do {
+      let a = number % 16;
+      remainders.unshift(a);
+  
+      number = Math.trunc(number / 16);
+    } while (number > 0);
+  
+    remainders = remainders.map(number => {
+      if (number === 10) return "A";
+      else if (number === 11) return "B";
+      else if (number === 12) return "C";
+      else if (number === 13) return "D";
+      else if (number === 14) return "E";
+      else if (number === 15) return "F";
+      else return number;
+    });
+  
+    return remainders.join("");
+  };
+
+  if (arguments.length === 3) {
+    const hexcode = [...arguments].map(argument => {
+      if (argument > 255) return "FF";; // normalize greater than values to 255
+      if (argument < 0) return "00"; // normalize lesser than values to 0
+
+      let result = toHex(argument);
+      if (result.toString().length < 2) return `0${result}` // make the string a double length string
+      else return result;
+    });
+    return hexcode.join("");
+  } else return null;
+};
