2f7fa127be1f8123fe7269046b59272177b28042
eduek <eduekakpannyah@gmail.com>
2023-01-25 16:59:27 +0100

    fix: correct solution to pete the baker kata

--- peteTheBaker/peteTheBaker.js
+++ peteTheBaker/peteTheBaker.js
@@ -1,25 +1,34 @@
 function cakes(recipe, available) {
   const ingredients = Object.keys(recipe);
+
+  // all ingredients checks that all items in the recipe is present in the given array
   const isAllIngredients = ingredients.every(item => Object.keys(available).includes(item));
 
   let cake = 0;
 
   if (isAllIngredients) {
-    const totalQuantity = ingredients.reduce((prevIngredient, nextIngredient) => prevIngredient + recipe[nextIngredient], 0);
+    const recipeQuantity = ingredients.reduce((prevIngredient, nextIngredient) => prevIngredient + recipe[nextIngredient], 0);
 
-    // availableIngredients.
+    let x = 1;
 
-    {
-      a
+    while (x > 0) {
+      let fullCake = 0;
+
+      ingredients.forEach(ingredient => {
+        if (available[ingredient] >= recipe[ingredient]) {
+          fullCake = fullCake + recipe[ingredient];
+          available[ingredient] = available[ingredient] - recipe[ingredient];
+        } else return;
+      });
+
+      if (fullCake === recipeQuantity) {
+        cake++;
+      } else break;
     }
-
-    // ingredients.forEach(ingredient => {
-    //   let quantity = available[]
-    // })
-    console.log(totalQuantity)
-  } else return 0;
-
-  console.log(ingredients, isAllIngredients); 
+  }
+  
+  return cake;
 }
 
-cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+console.log("cakes: " + cakes({flour: 0, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
